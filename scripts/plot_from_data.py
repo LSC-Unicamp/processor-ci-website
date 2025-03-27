ef01d15d7c13bad5c5a6fdf055a322eace72eed8
Packss <enzoana@hotmail.com>
2025-03-27 11:45:30 -0300

    Fix formatting

--- scripts/plot_from_data.py
+++ scripts/plot_from_data.py
@@ -14,38 +14,38 @@ class SynthData:
 
     def load_data(self, path):
         try:
-            with open(path, "r") as f:
+            with open(path, 'r') as f:
                 self.data = json.load(f)
-            self.processor = self.data["processor"]
-            self.board = self.data["board"]
-            self.luts = self.data["luts"]
-            self.used_luts = self.luts.pop("used")
-            self.max_freq = int(self.data["max_freq_mhz"])
+            self.processor = self.data['processor']
+            self.board = self.data['board']
+            self.luts = self.data['luts']
+            self.used_luts = self.luts.pop('used')
+            self.max_freq = int(self.data['max_freq_mhz'])
         except Exception as e:
-            print(f"File {path} could not be loaded: {e}")
+            print(f'File {path} could not be loaded: {e}')
             raise e
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # create a plot with frequency vs luts with all json files in the directory
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument(
-        "--path",
-        "-p",
-        help="Path to the directory containing the json files",
-        default=".",
+        '--path',
+        '-p',
+        help='Path to the directory containing the json files',
+        default='.',
     )
     data = []
     for file in os.listdir(arg_parser.parse_args().path):
-        if file.endswith(".json"):
+        if file.endswith('.json'):
             try:
                 synth = SynthData()
                 synth.load_data(file)
                 data.append(synth)
             except Exception as e:
-                print(f"Error processing {file}: {e}")
+                print(f'Error processing {file}: {e}')
     if not data:
-        print("No files were processed")
+        print('No files were processed')
         exit(1)
     # fig = px.scatter(
     #     x=[x.max_freq for x in data],
@@ -65,11 +65,11 @@ if __name__ == "__main__":
             x=[x.max_freq for x in data if x.board == board],
             y=[x.used_luts for x in data if x.board == board],
             text=[x.processor for x in data if x.board == board],
-            labels={"x": "Frequency (MHz)", "y": "Used LUTs"},
-            title=f"{board} Frequency vs LUTs",
+            labels={'x': 'Frequency (MHz)', 'y': 'Used LUTs'},
+            title=f'{board} Frequency vs LUTs',
             color=[x.processor for x in data if x.board == board],
         )
-        fig.update_traces(textposition="top center")
-        fig.update_layout(legend_title_text="Processor")
-        fig.write_html(f"performance_comparison_{board}.html")
-        fig.write_image(f"performance_comparison_{board}.png")
+        fig.update_traces(textposition='top center')
+        fig.update_layout(legend_title_text='Processor')
+        fig.write_html(f'performance_comparison_{board}.html')
+        fig.write_image(f'performance_comparison_{board}.png')
